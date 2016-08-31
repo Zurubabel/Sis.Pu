d651c546fd9187ae2632c452c80a4442dc3eb63d
Aulas <Aulas>
2016-08-31 17:32:40 -0300

    Aula 5 - Criando as tabelas de tipos de trabalho

--- banco_dados/01_Criação_Bases.sql
+++ banco_dados/01_Criação_Bases.sql
@@ -12,7 +12,7 @@ CREATE TABLE tb_Profissional (
 	dtInclusao DATE,
 	dtAlteracao DATE
 	/* Tipos de Trabalho ?*/
-)
+);
 
 CREATE TABLE tb_TipoSexo (
 	cdTipoSexo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -20,4 +20,25 @@ CREATE TABLE tb_TipoSexo (
 	bAtivo BOOLEAN NOT NULL DEFAULT TRUE,
 	dtInclusao DATE,
 	dtAlteracao DATE
-)
+);
+
+INSERT INTO tb_TipoSexo (nmTipoSexo, dtInclusao) VALUES ("Heterossexual", NOW()); 
+INSERT INTO tb_TipoSexo (nmTipoSexo, dtInclusao) VALUES ("Bissexual", NOW());
+INSERT INTO tb_TipoSexo (nmTipoSexo, dtInclusao) VALUES ("Homosexual", NOW());
+
+CREATE TABLE tb_ProfissionalTipoTrabalho (
+	cdProfissionalTipoTrabalho INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	idProfissional INT NOT NULL,
+	cdTipoTrabalho INT NOT NULL,
+	bAtivo BOOLEAN NOT NULL DEFAULT TRUE,
+	dtInclusao DATE,
+	dtAlteracao DATE
+);
+
+CREATE TABLE tb_TipoTrabalho (
+	cdTipoTrabalho INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nmTipoTrabalho VARCHAR(50) NOT NULL,
+	bAtivo BOOLEAN NOT NULL DEFAULT TRUE,
+	dtInclusao DATE,
+	dtAlteracao DATE
+);
