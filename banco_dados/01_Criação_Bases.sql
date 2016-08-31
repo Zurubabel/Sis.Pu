bf6a48080fc53c03c5e8c502082bf9dbca6f4576
Aulas <Aulas>
2016-08-31 16:56:14 -0300

    Aula 4 - Base de relacionamentos das(dos) profissionais

--- banco_dados/01_Criação_Bases.sql
+++ banco_dados/01_Criação_Bases.sql
@@ -9,5 +9,15 @@ CREATE TABLE tb_Profissional (
 	nmSexo VARCHAR(2),
 	qtBusto FLOAT,
 	qtBenga FLOAT,
+	dtInclusao DATE,
+	dtAlteracao DATE
 	/* Tipos de Trabalho ?*/
+)
+
+CREATE TABLE tb_TipoSexo (
+	cdTipoSexo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nmTipoSexo VARCHAR(50) NOT NULL,
+	bAtivo BOOLEAN NOT NULL DEFAULT TRUE,
+	dtInclusao DATE,
+	dtAlteracao DATE
 )
