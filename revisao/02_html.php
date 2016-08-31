d61a7a72d553961279974b40e3a23910d3d9fc5c
Aulas <Aulas>
2016-08-31 15:59:47 -0300

    Aula 2 - Revisão HTML

---
+++ revisao/02_html.php
@@ -0,0 +1,19 @@
+<!doctype html> <!-- "Identificação de tipo de página" -->
+<html> <!-- Tag principal da página -->
+	<head> <!-- Cabe�a do site -->
+		<title>Página teste HTML</title>
+		
+		<meta charset="utf-8"> <!-- Codificação da página -->
+	</head>
+	<body>
+		<!-- Parágrafo -->
+		<p>Iausgdhduaisd</p>
+		<!-- Cabeçalhos h1 até h6 -->
+		<h1>h1</h1>
+		<h2>h2</h2>
+		<h3>h3</h3>
+		<h4>h4</h4>
+		<h5>h5</h5>
+		<h6>h6</h6>
+	</body>
+</html>
