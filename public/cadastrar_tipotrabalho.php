3fe9b95e5b07b52d33db8fe99bc37bc027a11b53
Aulas <Aulas>
2016-09-29 01:04:40 -0300

    Aula 10 - Criação do Formulário de Cadastro de Tipo de Trabalho

---
+++ public/cadastrar_tipotrabalho.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+	<head>
+		<title>SIS.PU - Cadastro de Tipo de Trabalho</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<div id="divCadastro">
+			<form>
+				<ul>
+					<li><h3>Cadastro de Tipo de Trabalho</h3></li>
+				</ul>
+				<ul>
+					<li>C&oacute;digo</li>
+					<li><input type="number" name="cdTipoTrabalho" /> </li>
+				</ul>
+				<ul>
+					<li>Nome do Trabalho</li>
+					<li><input type="text" maxlength="50" name="nmTipoTrabalho" /></li>
+				</ul>
+				<ul>
+					<li>Ativo: <input type="checkbox" name="bAtivo" /></li>
+				</ul>
+				<ul>
+					<li><button type="submit">Enviar</button>
+				</ul>
+			</form>
+		</div>
+	</body>
+</html>
+
+
